8c8d084243968b05ad75c3e1e67373581275c7ba
jeshi2 <jamesvean19@gmail.com>
2023-08-02 22:09:19 +0300

    jeshi2

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -6,15 +6,26 @@
  * @second: pointer to the second node
  * Return: pointer to the lowest common ancestor or NULL if not found
  */
+
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-	if (!first || !second)
-		return (NULL);
-	if (first == second)
-		return ((binary_tree_t *)first);
-	if (first == second->parent || second == first->parent)
-		return (first->parent);
-	if (first->parent == second->parent)
-		return (first->parent);
-	return (binary_trees_ancestor(first->parent, second->parent));
+	if (first == NULL || second == NULL)
+        	return (NULL);
+
+    	const binary_tree_t *current1 = first;
+    	while (current1 != NULL)
+    	{
+        	const binary_tree_t *current2 = second;
+        	while (current2 != NULL)
+        	{
+            		if (current1 == current2)
+                		return ((binary_tree_t *)current1);
+
+            		current2 = current2->parent;
+      		}
+
+       		current1 = current1->parent;
+    	}
+
+ 	return (NULL);
 }
