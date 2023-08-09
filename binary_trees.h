f97c16292e8e15bcd6045586309fefa0f1624b92
jeshi2 <jamesvean19@gmail.com>
2023-08-09 22:20:28 +0300

    mid

--- binary_trees.h
+++ binary_trees.h
@@ -68,9 +68,11 @@ int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
+bst_t *bst_remove(bst_t *root, int value);
 
 /*helper function*/
 int  is_bst_recursive(const binary_tree_t *node, int min, int max);
+bst_t *bst_min_value_node(bst_t *node);
 
 
 #endif /*BINARY_TREE_H*/
