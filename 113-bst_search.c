87c7f790b7912cc2c261058b61c89922e6a455dd
Jane Fadhili Njaga <janefadhili@gmail.com>
2023-08-09 12:24:57 -0700

    betty

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -14,7 +14,7 @@ bst_t *bst_search(const bst_t *tree, int value)
 	if (value == tree->n)
 		return ((bst_t *)tree);
 	if (value < tree->n)
-	       return (bst_search(tree->left, value));
+		return (bst_search(tree->left, value));
 	else
 		return (bst_search(tree->right, value));
 }	
