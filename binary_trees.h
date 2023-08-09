fbb463ee821d5891547a246ef36aeef58c69f729
jeshi2 <jamesvean19@gmail.com>
2023-08-09 21:24:36 +0300

    makefile

--- binary_trees.h
+++ binary_trees.h
@@ -66,6 +66,7 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
 
 /*helper function*/
 int  is_bst_recursive(const binary_tree_t *node, int min, int max);
